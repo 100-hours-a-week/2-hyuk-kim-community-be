35da82eade4f37e75ac16f38adbfe0151fa3628b
sanghyuk Kim <tkdgur5031@naver.com>
2024-11-06 04:59:05 +0900

    fix: 게시글 삭제 기능 구현 -> 댓글 삭제 기능 추가 필요

--- Service/BoardService.js
+++ Service/BoardService.js
@@ -3,34 +3,34 @@ const comment = require('../Model/Comment');
 const {json} = require("express");
 
 
-module.exports.getPostList = async (req, res) => {
+module.exports.getPostList = async (req) => {
 
 }
 
-module.exports.createPost = async (req, res) => {
+module.exports.createPost = async (req) => {
     return post.createPost(req.body.title, req.body.content, req.body.email);
 }
 
-module.exports.getPostByPostId = async (req, res) => {
+module.exports.getPostByPostId = async (req) => {
     const result = await post.getPostByPostId(req.params.postId);
     result["postId"] = req.params.postId;
     result["comment"] = await comment.getCommentListByPostId(req.params.postId) || "";
     return result;
 }
 
-module.exports.patchPostViews = async (req, res) => {
+module.exports.patchPostViews = async (req) => {
     return post.addPostViews(req.params.postId);
 }
 
-module.exports.updatePostByPostId = async (req, res) => {
+module.exports.updatePostByPostId = async (req) => {
 
 }
 
-module.exports.deletePostByPostId = async (req, res) => {
+module.exports.deletePostByPostId = async (req) => {
     return await post.deleteById(req.params.postId);
 }
 
-module.exports.createComment = async (req, res) => {
+module.exports.createComment = async (req) => {
 
 }
 
