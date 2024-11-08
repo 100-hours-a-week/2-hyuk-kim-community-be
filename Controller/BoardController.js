3dec53d55e07726b455cbfd21c1d28a470616024
sanghyuk Kim <tkdgur5031@naver.com>
2024-11-08 14:53:44 +0900

    feat: v0.1.0 기본 기능 완성

--- Controller/BoardController.js
+++ Controller/BoardController.js
@@ -20,33 +20,41 @@ module.exports.getPostByPostId = async (req, res) => {
 }
 
 module.exports.getPostEditByPostId = async (req, res) => {
+    console.log("get post start!!: ", req.params.postId);
     return res.status(200).json(await boardService.getPostEditByPostId(req));
 }
 
 module.exports.patchPostViews = async (req, res) => {
+    console.log("patch post start!!: ", req.params.postId);
     return res.status(200).json(await boardService.patchPostViews(req));
 }
 
 module.exports.updatePostByPostId = async (req, res) => {
+    console.log("update post start!!: ", req.params.postId);
     return res.status(200).json(await boardService.updatePostByPostId(req));
 }
 
 module.exports.deletePostByPostId = async (req, res) => {
+    console.log("delete post start!!: ", req.params.postId);
     return res.status(200).json(await boardService.deletePostByPostId(req));
 }
 
 module.exports.createComment = async (req, res) => {
+    console.log("create comment start!!: ", req.params.commentId);
     return res.status(200).json(await boardService.createComment(req));
 }
 
 module.exports.getCommentListByPostId = async (req, res) => {
+    console.log("get comment list start!!: ", req.params.commentId);
     return res.status(200).json(await boardService.getCommentListByPostId(req));
 }
 
 module.exports.updateCommentByCommentId = async (req, res) => {
+    console.log("update comment start!!: ", req.params.commentId);
     return res.status(200).json(await boardService.updateCommentByCommentId(req));
 }
 
 module.exports.deleteCommentByCommentId = async (req, res) => {
+    console.log("delete comment start!!: ", req.params.commentId);
     return res.status(200).json(await boardService.deleteCommentByCommentId(req));
 }
