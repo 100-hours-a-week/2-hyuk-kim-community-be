9b753f4800ad377c43e0b6b31b9de425be42bd89
sanghyuk Kim <tkdgur5031@naver.com>
2024-11-12 11:08:52 +0900

    fix: apiResponse.js 500 에러 발생시 오류 발생 문제 수정 merge: README.md 최신화

--- utils/apiResponse.js
+++ utils/apiResponse.js
@@ -1,17 +1,19 @@
 const dateManager = require("./dateManager");
+const userResponseCodes = require("./userResponseCodes");
 
 module.exports.success = (req, res, response) => {
   console.log(
     `info!! : [${dateManager.getCurrentFormattedDate()}] ${req.method} ${req.originalUrl} - Status: ${response.code}, Message: ${response.message}`,
   );
-  res
-    .status(response.code)
-    .json({ message: response.message, data: response.data || {} });
+  return res.status(response.code).json({ message: response.message, data: response.data || {} });
 };
 
 module.exports.error = (req, res, response) => {
   console.log(
     `error!! : [${dateManager.getCurrentFormattedDate()}] ${req.method} ${req.originalUrl} - Status: ${response.code}, Message: ${response.message}`,
   );
-  res.status(response.code).json({ message: response.message });
+  if (response.code >= 500) {
+    return res.status(response.code).json({ message: userResponseCodes.SERVER_ERROR.unexpectedError.message });
+  }
+  return res.status(response.code).json({ message: response.message });
 };
