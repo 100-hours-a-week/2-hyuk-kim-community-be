650957992263bb3b0a360e3d836a5a322459ee9f
sanghyuk Kim <tkdgur5031@naver.com>
2024-11-19 05:10:44 +0900

    fix: code convention 적용

--- common/errors/CustomError.js
+++ common/errors/CustomError.js
@@ -1,46 +1,45 @@
 class CustomError extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.name = this.constructor.name;
-        this.statusCode = statusCode || 500;
-    }
+  constructor(message, statusCode) {
+    super(message);
+    this.name = this.constructor.name;
+    this.statusCode = statusCode || 500;
+  }
 }
 
 class BadRequestError extends CustomError {
-    constructor(message = 'Bad Request') {
-        super(message, 400);
-    }
+  constructor(message = "Bad Request") {
+    super(message, 400);
+  }
 }
 
-
 class UnauthorizedError extends CustomError {
-    constructor(message = 'Unauthorized') {
-        super(message, 401);
-    }
+  constructor(message = "Unauthorized") {
+    super(message, 401);
+  }
 }
 
 class NotFoundError extends CustomError {
-    constructor(message = 'Resource not found') {
-        super(message, 404);
-    }
+  constructor(message = "Resource not found") {
+    super(message, 404);
+  }
 }
 
 class ConflictError extends CustomError {
-    constructor(message = 'Conflict') {
-        super(message, 409);
-    }
+  constructor(message = "Conflict") {
+    super(message, 409);
+  }
 }
 
 class InternalServerError extends CustomError {
-    constructor(message = 'Internal Server Error') {
-        super(message, 500);
-    }
+  constructor(message = "Internal Server Error") {
+    super(message, 500);
+  }
 }
 
 module.exports = {
-    BadRequestError,
-    UnauthorizedError,
-    NotFoundError,
-    ConflictError,
-    InternalServerError,
+  BadRequestError,
+  UnauthorizedError,
+  NotFoundError,
+  ConflictError,
+  InternalServerError,
 };
--- common/responses/apiResponse.js
+++ common/responses/apiResponse.js
@@ -2,13 +2,11 @@ const dateManager = require("../../utils/dateManager");
 
 module.exports.success = (req, res, response) => {
   console.log(
-      `info!! : [${dateManager.getCurrentFormattedDate()}] ${req.method} ${req.originalUrl} - Status: ${response.statusCode}, Message: ${response.message}`,
+    `info!! : [${dateManager.getCurrentFormattedDate()}] ${req.method} ${req.originalUrl} - Status: ${response.statusCode}, Message: ${response.message}`,
   );
-  return res
-      .status(response.statusCode)
-      .json({
-        status: 'success',
-        message: response.message,
-        data: response.data || {}
-      });
+  return res.status(response.statusCode).json({
+    status: "success",
+    message: response.message,
+    data: response.data || {},
+  });
 };
