dcc534f52e45de6af3f11091a2310c2a39fd79ab
sanghyuk Kim <tkdgur5031@naver.com>
2024-11-19 02:58:17 +0900

    fix: userRoute 코드 형식 통일 - userErrorCode.js를 활용한 에러 처리 형식 통일 - userSuccessCode.js를 활용한 응답 형식 통일

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,56 +1,80 @@
 const userService = require("../Service/UserService");
-const apiResponse = require("../utils/apiResponse");
+const apiResponse = require("../common/responses/apiResponse");
+const UserSuccessCode = require("../common/responses/UserSuccessCode");
 const {generateRefreshToken, generateAccessToken} = require("../utils/provider/jwtProvider");
 const {setAccessToken} = require("../utils/provider/cookieProvider");
+
+/*
+반복되는 코드가 많아 추상화로 줄일 수 있을 거 같은데, 그게 올바른 방식인지 모르겠네!
+추가될만한 요소
+1. Validation -> 근데 이거는 미들웨어에서 Joi라는 라이브러리 사용하면 충분히 추상화가 가능해보임!
+2. Session Cookie -> 이게 좀 문제긴 하네 이거는 느낌이 좀 다를듯 -> 근데 그건 결국 로그인만 그런 건데
+                  -> 검증은 미들웨어에서 진행해서 컨트롤러는 관련 없는듯!
+
+=> 그간의 경험 내에서 추상화가 안 될만한 요소는 로그인 뿐인 거 같음
+=> 2주차 과제 제출하고나서 한번 테스트 해보고 감 잡아보기!
+*/
 
 module.exports.login = async function (req, res, next) {
   try {
     const result = await userService.login(req);
-
-    apiResponse.success(req, res, result);
+    const successResponse = UserSuccessCode.createLoginSuccess(result);
+    apiResponse.success(req, res, successResponse);
   } catch (error) {
-    console.log(error);
-    next(error)
+    next(error);
   }
 };
 
-module.exports.logout = function (req, res) {};
+module.exports.logout = function (req, res, next) {};
 
-module.exports.signup = async function (req, res) {
+module.exports.signup = async function (req, res, next) {
   try {
-    const response = await userService.signup(req);
-    apiResponse.success(req, res, response);
+    const result = await userService.signup(req);
+    const successResponse = UserSuccessCode.createUserCreated(result);
+    apiResponse.success(req, res, successResponse);
   } catch (error) {
-    apiResponse.error(req, res, error);
+    next(error);
   }
 };
 
-module.exports.signout = async function (req, res) {
+module.exports.signout = async function (req, res, next) {
   try {
-    const id = await userService.signout(req);
-    res.status(200).json(id);
+    const result = await userService.signout(req);
+    const successResponse = UserSuccessCode.createAccountDeleted(result);
+    apiResponse.success(req, res, successResponse);
   } catch (error) {
-    apiResponse.error(req, res, error);
+    next(error);
   }
 };
 
-module.exports.getNicknameByEmail = async function (req, res) {
+module.exports.getNicknameByEmail = async function (req, res, next) {
   try {
-    const nickname = await userService.getNicknameByEmail(req);
-    res.status(200).json(nickname);
+    const result = await userService.getNicknameByEmail(req);
+    const successResponse = UserSuccessCode.createUserFetched(result);
+    apiResponse.success(req, res, successResponse);
   } catch (error) {
-    apiResponse.error(req, res, error);
+    next(error);
   }
 };
 
-module.exports.updateNicknameByEmail = async function (req, res) {
-  const id = await userService.updateNicknameEmail(req);
-  res.status(200).json(id);
+module.exports.updateNicknameByEmail = async function (req, res, next) {
+  try {
+    const result = await userService.updateNicknameEmail(req);
+    const successResponse = UserSuccessCode.createNicknameUpdated(result);
+    apiResponse.success(req, res, successResponse);
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports.updatePasswordByEmail = async function (req, res) {
-  const id = await userService.updatePasswordEmail(req);
-  res.status(200).json(id);
+module.exports.updatePasswordByEmail = async function (req, res, next) {
+  try {
+    const result = await userService.updatePasswordEmail(req);
+    const successResponse = UserSuccessCode.createPasswordUpdated(result);
+    apiResponse.success(req, res, successResponse);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = this;
--- Service/UserService.js
+++ Service/UserService.js
@@ -1,47 +1,81 @@
 const User = require("../Model/user");
-const UserErrorCode = require("../utils/userErrorCode");
+const UserErrorCode = require("../common/errors/userErrorCode");
+
+/*
+검증 로직 추가 필요함!
+-> 지금은 값이 존재하는지 보는데, 비밀번호 몇글자 이런 식으로 수정이 필요함!
+ */
+
+
 
 module.exports.login = async (req) => {
-  const loginSuccess = await User.login(req.body.email, req.body.password);
-  if (!loginSuccess) {
+  const { email, password } = req.body;
+  validateFields(req.body, ['email', 'password']);
+  const user = await validateEmail(email);
+  const passwordMatch = user.password === password; // 패스워드 비교하는 함수 필요!
+  if (!passwordMatch) {
     throw UserErrorCode.createInvalidCredentials();
   }
-  return userResponseCodes.createResponse(
-    userResponseCodes.SUCCESS.loginSuccess,
-    req.body.email,
-  );
+  return {email}
 };
 
 module.exports.logout = async (req) => {};
 
 module.exports.signup = async (req) => {
   const { email, password, nickname } = req.body;
-  if (!email || !password) throw userResponseCodes.BAD_REQUEST.invalidFormat;
-  if (await User.findUserByEmail(email)) {
-    throw userResponseCodes.CONFLICT.emailExists;
-  }
-  const data = await User.signup(email, password, nickname);
-  return userResponseCodes.createResponse(
-    userResponseCodes.CREATED.userCreated,
-    data,
-  );
+  validateFields(req.body, ['email', 'password', 'nickname']);
+  await validateNewEmail(email); // 얘는 반대여야지!
+  return await User.signup(email, password, nickname);
 };
 
 module.exports.signout = async (req) => {
-  return User.signout(req.body.email);
+  const { email } = req.body;
+  validateFields(req.body, ['email']);
+  await validateEmail(email);
+  return User.signout(email);
 };
 
 module.exports.getNicknameByEmail = async (req) => {
-  return User.getNicknameByEmail(req.params.email);
+  const email = req.body.email;
+  validateFields(req.body, ['email']);
+  await validateEmail(email);
+  return User.getNicknameByEmail(email);
 };
 
 module.exports.updateNicknameEmail = async (req) => {
-  return User.updateNicknameEmail(req.body.email, req.body.nickname);
+  const { email, nickname } = req.body;
+  validateFields(req.body, ['email', 'nickname']);
+  await validateEmail(email);
+  return User.updateNicknameEmail(email, nickname);
 };
 
 module.exports.updatePasswordEmail = async (req) => {
-  return User.updatePasswordEmail(req.body.email, req.body.password);
+  const { email, password } = req.body;
+  validateFields(req.body, ['email', 'password']);
+  await validateEmail(email);
+  return User.updatePasswordEmail(email, password);
 };
+
+// 필수값 데이터 검증
+const validateFields = (fields, required) => {
+  for (const field of required) {
+    if (!fields[field]) throw UserErrorCode.createInvalidFormat();
+  }
+};
+
+// 이메일에 맞는 사용자 있는지 검증
+const validateEmail = async (email) => {
+  const user = await User.validEmail(email);
+  if (!user) throw UserErrorCode.createUserNotFound();
+  return user;
+};
+
+// 새로운 이메일인지 검증
+const validateNewEmail = async (email) => {
+  const exists = await User.validEmail(email);
+  if (exists) throw UserErrorCode.createEmailExists();
+};
+
 
 // board (post, comment)를 받아 email에 맞는 닉네임을 추가하는 과정
 module.exports.setUserInfoByEmail = async (board) => {
