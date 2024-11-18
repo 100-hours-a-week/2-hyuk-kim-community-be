0b3379e1b6046f57b32adaaad68c69bcff7eddaf
sanghyuk Kim <tkdgur5031@naver.com>
2024-11-19 05:10:07 +0900

    feat: 응답 형식 통일 및 테스트 완료

--- Route/BoardRoute.js
+++ Route/BoardRoute.js
@@ -2,11 +2,12 @@ const express = require("express");
 const boardRoute = express.Router();
 const boardController = require("./../Controller/BoardController.js");
 const authMiddleware = require("./../middlewares/authMiddleware.js");
+//authMiddleware,
+boardRoute.get("/posts", boardController.getPostList); // O
+boardRoute.get("/posts/:postId", boardController.getPostByPostId); // O
+boardRoute.get("/posts/:postId/edit", boardController.getPostEditByPostId); // O
 
-boardRoute.get("/posts", authMiddleware, boardController.getPostList);
 boardRoute.post("/posts", boardController.createPost);
-boardRoute.get("/posts/:postId", boardController.getPostByPostId);
-boardRoute.get("/posts/:postId/edit", boardController.getPostEditByPostId);
 boardRoute.patch("/posts/:postId", boardController.updatePostByPostId);
 boardRoute.delete("/posts/:postId", boardController.deletePostByPostId);
 
