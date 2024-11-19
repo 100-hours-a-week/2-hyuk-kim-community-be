daf0a5df792abab906b0b50ebda7f0143036941e
sanghyuk Kim <tkdgur5031@naver.com>
2024-11-19 20:07:06 +0900

    feat: logout (session, cookie 삭제) 구현

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -12,8 +12,11 @@ const { setAccessToken } = require("../utils/provider/cookieProvider");
 반복되는 코드가 많아 추상화할 수 있을 거 같은데, 그게 긍정적인 방식인지 모르겠네!
 추가될만한 요소
 1. Validation -> 근데 이거는 미들웨어에서 Joi라는 라이브러리 사용하면 충분히 추상화가 가능해보임!
+    -> 어차피 이건 Service 아님 미들웨어에서 하는 게 적절하겠네!
 2. Session Cookie -> 이게 좀 문제긴 하네 이거는 느낌이 좀 다를듯 -> 근데 그건 결국 로그인만 그런 건데
                   -> 검증은 미들웨어에서 진행해서 컨트롤러는 관련 없는듯!
+
+=> 현재로서는 로그인시 Session 생성하는 거 제외하고 추상화가 가능한 것으로 보임 !
 
 => 그간의 경험 내에서 추상화가 안 될만한 요소는 로그인 뿐인 거 같음
 => 2주차 과제 제출하고나서 한번 테스트 해보고 감 잡아보기!
@@ -30,7 +33,12 @@ module.exports.login = async function (req, res, next) {
   }
 };
 
-module.exports.logout = function (req, res, next) {};
+module.exports.logout = function (req, res, next) {
+  req.session.destroy();
+  res.clearCookie('connect.sid');
+  const successResponse = UserSuccessCode.createLogoutSuccess();
+  apiResponse.success(req, res, successResponse );
+};
 
 module.exports.signup = async function (req, res, next) {
   try {
