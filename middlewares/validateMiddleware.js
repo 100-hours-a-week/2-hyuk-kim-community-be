d8a02f50928ddd183c876ac064bdd67b221d63d0
sanghyuk Kim <tkdgur5031@naver.com>
2025-02-04 15:03:34 +0900

    fix: validation 미들웨어 이름 수정 및 기존 ErrorResponse 사용하도록 수정

---
+++ middlewares/validateMiddleware.js
@@ -0,0 +1,24 @@
+import {BadRequestError} from "../common/codes/CustomError";
+
+export const validateMiddleware = (schema) => {
+    return (req, res, next) => {
+        try {
+            const { error } = schema.validate(req.body, {
+                abortEarly: false, // 모든 에러를 한번에 반환
+                stripUnknown: true // 정의되지 않은 속성 제거
+            });
+
+            if (error) {
+                const errorMessage = error.details
+                    .map((detail) => detail.message)
+                    .join(', ');
+
+                throw new BadRequestError(errorMessage);
+            }
+
+            next();
+        } catch (error) {
+            next(error);
+        }
+    };
+};
